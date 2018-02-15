0b892c424b76f61315d472ede8f062df6415f2f6
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2018-02-14 22:04:05 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -3,9 +3,31 @@ function Mostrar()
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+switch (mesDelAño)
+{
+    case "Julio":
+  
+    case "Agosto":
+    alert ("Abrigate que hace frio.");
+    break;
+
+    case "Septiembre":
+  
+
+    case "Octubre":
+  
+    case "Noviembre":
+  
+    case "Diciembre":
+    alert ("Ya pasamos el frio, ahora calor ");
+    break;
+
+    default :                               //si no es ninguna de las otras condiciones por defecto son estas 
+    alert ("Falta para el invierno");
+    break;
 
 
+}
 
 
 }//FIN DE LA FUNCIÓN
