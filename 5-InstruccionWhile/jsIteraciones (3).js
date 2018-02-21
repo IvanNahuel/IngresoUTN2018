9522ed9d70996c6310e026e9178d0ded8eeb665b
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2018-02-21 20:41:00 +0100

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -3,5 +3,15 @@ function Mostrar()
 
 var clave = prompt("ingrese el número clave.");
 
+while (!(clave == "utn750")&&clave !=null){
+
+    clave = prompt ("Reingrese clave");
+}
+
+if ( clave!=null)
+{
+    alert ("clave correcta");
+}
+
 
 }//FIN DE LA FUNCIÓN
