7d342d77fd6cba41d4f53ff178410ea52101032f
IvanNahuel <36246972+IvanNahuel@users.noreply.github.com>
2018-02-28 22:13:01 -0300

    Add files via upload

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,39 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+
+    var numero1 = parseFloat( document.getElementById("PrecioUno").value);
+    var numero2 = parseFloat( document.getElementById("PrecioDos").value);
+    var numero3 = parseFloat( document.getElementById("PrecioTres").value);
+
+    var resultado;
+
+    resultado = numero1 + numero2 + numero3;
+
+    alert (resultado);
 	
 }
 function Promedio () 
 {
-	
+    var numero1 = parseFloat( document.getElementById("PrecioUno").value);
+    var numero2 = parseFloat( document.getElementById("PrecioDos").value);
+    var numero3 = parseFloat( document.getElementById("PrecioTres").value);
+
+    var resultado;
+
+    resultado = numero1 + numero2 + numero3;
+
+    alert (resultado/3);
 }
 function PrecioFinal () 
 {
-	
+    var numero1 = parseFloat( document.getElementById("PrecioUno").value);
+    var numero2 = parseFloat( document.getElementById("PrecioDos").value);
+    var numero3 = parseFloat( document.getElementById("PrecioTres").value);
+
+    var resultado;
+
+    resultado = numero1 + numero2 + numero3;
+
+    alert (resultado/1.6);
 }
